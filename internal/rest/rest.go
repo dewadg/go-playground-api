519c778ea98b8014e55cb17d6096da08f9f2fc34
Dege <dewadg@dewadg.id>
2022-04-03 14:00:41 +0700

    fix static file routing

--- internal/rest/rest.go
+++ internal/rest/rest.go
@@ -3,14 +3,24 @@ package rest
 import (
 	"net/http"
 	"os"
+	"path"
+	"strings"
 
 	"github.com/go-chi/chi"
 )
 
 func Register(router chi.Router) error {
-	fs := http.FileServer(http.Dir(os.Getenv("WEB_DIR")))
+	webDir := os.Getenv("WEB_DIR")
+	fs := http.FileServer(http.Dir(webDir))
 
-	router.Handle("/*", fs)
+	router.Handle("/*", http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		if strings.Contains(request.URL.Path, ".") || request.URL.Path == "/" {
+			fs.ServeHTTP(writer, request)
+			return
+		}
+
+		http.ServeFile(writer, request, path.Join(webDir, "/index.html"))
+	}))
 
 	return nil
 }
