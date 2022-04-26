023b5873483ebcf0c7cbd829dd8ca93619ffe2cd
Dege <dewadg@dewadg.id>
2022-04-26 16:15:14 +0700

    connect grpc client on client register

--- internal/grpc/gprc_orchestrator_service.go
+++ internal/grpc/gprc_orchestrator_service.go
@@ -3,15 +3,20 @@ package grpc
 import (
 	"context"
 	"fmt"
+	"sync"
 
 	"github.com/dewadg/go-playground-api/pkg/pb"
+	"github.com/google/uuid"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/peer"
 )
 
 type orchestratorService struct {
 	pb.UnimplementedOrchestratorServiceServer
 
-	clients map[string]pb.ExecutionServiceClient
+	clientsLock sync.Mutex
+	clients     map[string]pb.ExecutionServiceClient
 }
 
 func newOrchestratorService() *orchestratorService {
@@ -24,7 +29,23 @@ func (svc *orchestratorService) Register(ctx context.Context, request *pb.Regist
 	p, _ := peer.FromContext(ctx)
 	clientAddr := p.Addr.String()
 
+	conn, err := grpc.DialContext(
+		ctx,
+		clientAddr,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	clientID := uuid.New().String() + ":" + request.GetId()
+	client := pb.NewExecutionServiceClient(conn)
+
+	svc.clientsLock.Lock()
+	svc.clients[clientID] = client
+	svc.clientsLock.Unlock()
+
 	return &pb.RegisterResponse{
-		Message: fmt.Sprintf("%s: registered", clientAddr),
+		Message: fmt.Sprintf("%s: registered", clientID),
 	}, nil
 }
